5e697e2f8348ff5602146b99f0e2783b11dbddeb
Gidsss <88811389+Gidsss@users.noreply.github.com>
2024-05-09 15:47:30 +0800

    coupons
    create and edit coupons (also display of coupons tab)

--- lib/screens/admin/manage/addnewcoupons.dart
+++ lib/screens/admin/manage/addnewcoupons.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart'; // For date formatting
 import '../../../widgets/CustomFooterHeaderWidgets/adminfooter.dart';
 import '../../../widgets/CustomFooterHeaderWidgets/header2.dart';
+import '../../../widgets/CustomWidgets/UniversalButton.dart';
 
 class AddNewCouponsScreen extends StatefulWidget {
   const AddNewCouponsScreen({super.key});
@@ -10,6 +13,71 @@ class AddNewCouponsScreen extends StatefulWidget {
 }
 
 class _AddNewCouponsScreenState extends State<AddNewCouponsScreen> {
+  final TextEditingController couponCodeController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController benefitsController = TextEditingController();
+  final TextEditingController usageLimitController = TextEditingController();
+  final TextEditingController startDateController = TextEditingController();
+  final TextEditingController endDateController = TextEditingController();
+  String selectedStatus = 'Available';
+  final DateFormat dateFormat = DateFormat('MM/dd/yyyy');
+
+  Future<void> selectDate(
+      BuildContext context, TextEditingController controller) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null) {
+      setState(() {
+        controller.text = dateFormat.format(picked);
+      });
+    }
+  }
+
+  Future<void> addCoupon() async {
+    String couponCode = couponCodeController.text;
+    String description = descriptionController.text;
+    String benefits = benefitsController.text;
+    int usageLimit = int.tryParse(usageLimitController.text) ?? 0;
+    String startDate = startDateController.text;
+    String endDate =
+        endDateController.text.isNotEmpty ? endDateController.text : '';
+
+    if (couponCode.isEmpty ||
+        description.isEmpty ||
+        startDate.isEmpty ||
+        usageLimit == 0) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Please fill all required fields with valid data.')));
+      return;
+    }
+
+    // Create the coupon data to be stored in Firestore
+    Map<String, dynamic> couponData = {
+      'couponCode': couponCode,
+      'description': description,
+      'benefits': benefits,
+      'usageLimit': usageLimit,
+      'status': selectedStatus,
+      'startDate': startDate,
+      'endDate': endDate,
+    };
+
+    try {
+      await FirebaseFirestore.instance.collection('coupons').add(couponData);
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Coupon added successfully!')));
+      // Navigate back to the previous screen
+      Navigator.pop(context);
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Error adding coupon: $e')));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,13 +87,122 @@ class _AddNewCouponsScreenState extends State<AddNewCouponsScreen> {
         children: [
           Expanded(
             child: SingleChildScrollView(
-
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Image.asset(
+                        'assets/images/AllCouponsIcon.png', // Asset icon
+                        width: 88,
+                        height: 88,
+                      ),
+                    ),
+                    const Text(
+                      "Coupon Code Name",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    createField("", couponCodeController),
+                    const Text(
+                      "Description",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    createField("", descriptionController, maxLines: 3),
+                    const Text(
+                      "Benefits",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    createField("", benefitsController),
+                     const Text(
+                      "Usage Limit (Optional)",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    createField("", usageLimitController),
+                      const Text(
+                      "Status",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    DropdownButtonFormField<String>(
+                      value: selectedStatus,
+                      items: ['Available', 'Unavailable']
+                          .map((status) => DropdownMenuItem(
+                                value: status,
+                                child: Text(status),
+                              ))
+                          .toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedStatus = value!;
+                        });
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: '',
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    const Text(
+                      "Start Date",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    buildDatePickerField("", startDateController),
+                    const Text(
+                      "End Date (Optional)",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    buildDatePickerField(
+                        "", endDateController),
+                    const SizedBox(height: 15),
+                    CreateButton(
+                      buttontext: 'Add Coupon',
+                      navigator: addCoupon,
+                      context: context,
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
           AdminFooter(
-              buttonStatus: const [false, false, true, false, false],
-              context: context)
+            buttonStatus: const [false, false, true, false, false],
+            context: context,
+          )
         ],
+      ),
+    );
+  }
+
+  Widget createField(String label, TextEditingController controller,
+      {int maxLines = 1}) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15.0),
+      child: TextField(
+        controller: controller,
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          labelText: label,
+        ),
+      ),
+    );
+  }
+
+  Widget buildDatePickerField(String label, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15.0),
+      child: GestureDetector(
+        onTap: () => selectDate(context, controller),
+        child: AbsorbPointer(
+          child: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              labelText: label,
+              suffixIcon: const Icon(Icons.calendar_today),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/screens/admin/manage/allcoupons.dart
+++ lib/screens/admin/manage/allcoupons.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:project_v/screens/admin/manage/editcoupons.dart';
 import '../../../widgets/CustomFooterHeaderWidgets/adminfooter.dart';
 import '../../../widgets/CustomFooterHeaderWidgets/header2.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AllCouponsScreen extends StatefulWidget {
   const AllCouponsScreen({super.key});
@@ -9,7 +11,22 @@ class AllCouponsScreen extends StatefulWidget {
   State<AllCouponsScreen> createState() => _AllCouponsScreenState();
 }
 
-class _AllCouponsScreenState extends State<AllCouponsScreen> {
+class _AllCouponsScreenState extends State<AllCouponsScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,16 +34,148 @@ class _AllCouponsScreenState extends State<AllCouponsScreen> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          TabBar(
+            controller: _tabController,
+            tabs: const [
+              Tab(text: "Coupons"),
+              Tab(text: "Redeems"),
+              Tab(text: "Status"),
+            ],
+            indicatorColor: Colors.black,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey,
+          ),
           Expanded(
-            child: SingleChildScrollView(
-
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                buildCouponList(), // For the "Coupons" tab
+                buildRedeemsList(), // For the "Redeems" tab
+                buildStatusList(), // For the "Status" tab
+              ],
             ),
           ),
           AdminFooter(
-              buttonStatus: const [false, false, true, false, false],
-              context: context)
+            buttonStatus: const [false, false, true, false, false],
+            context: context,
+          )
         ],
       ),
+    );
+  }
+
+  // Coupons tab content dynamically from Firestore
+  Widget buildCouponList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('coupons').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(child: Text('No coupons available.'));
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((doc) {
+            Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+            String couponCode = data['couponCode'] ?? 'No Code';
+            String benefits = data['benefits'] ?? '';
+            String usageLimit = data['usageLimit'] != null
+                ? data['usageLimit'].toString()
+                : '---';
+            String status = data['status'] ?? 'Unknown';
+            String documentId = doc.id;
+
+            return Card(
+              child: ListTile(
+                leading: const Padding(
+                  padding: EdgeInsets.only(
+                      right: 24.0, top: 24.0), // Adjust the padding as needed
+                  child: SizedBox(
+                    width: 10, // Adjust the width as needed
+                    height: 75, // Adjust the height as needed
+                    child: Icon(Icons.local_offer,
+                        color: Colors.black,
+                        size: 35), // Adjust the size as needed
+                  ),
+                ),
+                title: Text(
+                  "$couponCode | $benefits",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 14),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Usage Limit: $usageLimit           Status: $status",
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w500, fontSize: 13),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4.0),
+                      child: Text("Start Date: ${data['startDate'] ?? '---'}",
+                          style: const TextStyle(fontSize: 12)),
+                    ),
+                    Text("End Date: ${data['endDate'] ?? '---'}",
+                        style: const TextStyle(fontSize: 12)),
+                  ],
+                ),
+                trailing: const Icon(Icons.arrow_forward_ios),
+                isThreeLine: true,
+                onTap: () {
+                  // Navigate to the edit screen
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EditCouponScreen(
+                        couponCode: couponCode,
+                        description: data['description'] ?? '',
+                        benefits: benefits,
+                        usageLimit: usageLimit,
+                        status: status,
+                        startDate: data['startDate'] ?? '',
+                        endDate: data['endDate'] ?? '',
+                        documentId: documentId,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+
+  Widget buildRedeemsList() {
+    
+    return ListView(
+      children: const [
+        ListTile(
+          title: Text("SUMMER GLASS"),
+          subtitle: Text("SUMMER Discount\nUsage Limit: 100"),
+          trailing: Icon(Icons.arrow_forward_ios),
+        ),
+        // Add more ListTiles or custom widgets as needed
+      ],
+    );
+  }
+
+  Widget buildStatusList() {
+
+    return ListView(
+      children: const [
+        ListTile(
+          title: Text("20240130YTK2"),
+          subtitle: Text("10% Discount\nStatus: Available"),
+          trailing: Icon(Icons.arrow_forward_ios),
+        ),
+        // Add more ListTiles or custom widgets as needed
+      ],
     );
   }
 }
