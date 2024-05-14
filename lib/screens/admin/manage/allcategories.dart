d1f9a416a085fb39f1cc2beb1ab0fe73095906ca
Miyukiin <149550452+Miyukiin@users.noreply.github.com>
2024-05-14 13:15:04 +0800

    Category Update Two

--- lib/screens/admin/manage/allcategories.dart
+++ lib/screens/admin/manage/allcategories.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import '../../../widgets/CustomFooterHeaderWidgets/adminfooter.dart';
 import '../../../widgets/CustomFooterHeaderWidgets/header2.dart';
 import 'package:project_v/screens/admin/manage/editcategory.dart';
@@ -12,7 +13,6 @@ class AllCategoryScreen extends StatefulWidget {
 }
 
 class _AllCategoryScreenState extends State<AllCategoryScreen> {
-
   // Define a method to create the table widget
   Widget buildDataTable(List<Map<String, dynamic>> data) {
     return Container(
@@ -20,14 +20,17 @@ class _AllCategoryScreenState extends State<AllCategoryScreen> {
         border: Border.all(color: Colors.black), // Add gray border
       ),
       child: DataTable(
-        columnSpacing: 40,
+        columnSpacing: (MediaQuery.of(context).size.width - 280) /
+            3, // Responsive data table size column
         headingRowHeight: 46, // Adjust the height of the heading row as needed
-        headingRowColor: MaterialStateColor.resolveWith((states) => Colors.black),
+        headingRowColor:
+            MaterialStateColor.resolveWith((states) => Colors.black),
         columns: [
           DataColumn(
             label: Container(
               color: Colors.black,
-              padding: const EdgeInsets.only(left: 0), // Remove horizontal padding
+              padding:
+                  const EdgeInsets.only(left: 0), // Remove horizontal padding
               alignment: Alignment.centerLeft, // Align text to the left
               child: const Text(
                 'Category Name',
@@ -39,7 +42,8 @@ class _AllCategoryScreenState extends State<AllCategoryScreen> {
           DataColumn(
             label: Container(
               color: Colors.black,
-              padding: const EdgeInsets.only(left: 0), // Remove horizontal padding
+              padding:
+                  const EdgeInsets.only(left: 0), // Remove horizontal padding
               alignment: Alignment.centerLeft, // Align text to the left
               child: const Text(
                 'Category Type', // Is parentcategory in firebase
@@ -51,7 +55,8 @@ class _AllCategoryScreenState extends State<AllCategoryScreen> {
           DataColumn(
             label: Container(
               color: Colors.black,
-              padding: const EdgeInsets.only(left: 0), // Remove horizontal padding
+              padding:
+                  const EdgeInsets.only(left: 0), // Remove horizontal padding
               alignment: Alignment.centerLeft, // Align text to the left
               child: const Text(
                 '   Edit',
@@ -64,34 +69,44 @@ class _AllCategoryScreenState extends State<AllCategoryScreen> {
         rows: data
             .map(
               (item) => DataRow(cells: [
-            DataCell(Text(item['category_name'].toString())),
-            DataCell(Text(item['category_type'].toString())),
-            DataCell(
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(20), // Adjust the radius for rounded corners
-                ),
-                padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 1), // Adjust the padding values
-                child: IconButton(
-                  icon: const Icon(Icons.edit, color: Colors.white), // Set icon color to white
-                  onPressed: () {
-                    // Pass data to the EditCategoryScreen when edit button is clicked
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => EditCategoryScreen(
-                          initialCategoryName: item['category_name'].toString(),
-                          initialCategoryType: item['category_type'].toString(),
+                DataCell(Text(item['category'].toString())),
+                DataCell(Text(item['parentcategory'].toString())),
+                DataCell(
+                  Align(
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 1.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 1,
+                            horizontal: 1), // Adjust the padding values
+                        child: IconButton(
+                          icon: const Icon(Icons.edit,
+                              color: Colors.white), // Set icon color to white
+                          onPressed: () {
+                            // Pass data to the EditCategoryScreen when edit button is clicked
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => EditCategoryScreen(
+                                  initialCategoryName:
+                                      item['category'].toString(),
+                                  initialCategoryType:
+                                      item['parentcategory'].toString(),
+                                ),
+                              ),
+                            );
+                          },
                         ),
                       ),
-                    );
-                  },
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ]),
-        )
+              ]),
+            )
             .toList(),
       ),
     );
@@ -99,20 +114,24 @@ class _AllCategoryScreenState extends State<AllCategoryScreen> {
 
   // Method to fetch data from Firestore or any other data source
   Future<List<Map<String, dynamic>>> fetchData() async {
-    List<Map<String, dynamic>> categoriesDict = [];
-    final db = FirebaseFirestore.instance;
-    final QuerySnapshot snapShot = await db.collection('categories').get();
+    try {
+      List<Map<String, dynamic>> categoriesDict = [];
+      final db = FirebaseFirestore.instance;
+      final QuerySnapshot snapShot = await db.collection('categories').get();
 
-    for (DocumentSnapshot document in snapShot.docs){
+      for (DocumentSnapshot document in snapShot.docs) {
         categoriesDict.add(document.data() as Map<String, dynamic>);
+      }
+
+      return categoriesDict;
+
+      // Fetch your data here and return it as a list of maps
+      // For example:
+      // final snapshot = await FirebaseFirestore.instance.collection('categories').get();
+      // return snapshot.docs.map((doc) => doc.data()).toList();
+    } catch (error) {
+      throw Exception("Failed to fetch data: $error");
     }
-
-    return categoriesDict;
-
-    // Fetch your data here and return it as a list of maps
-    // For example:
-    // final snapshot = await FirebaseFirestore.instance.collection('categories').get();
-    // return snapshot.docs.map((doc) => doc.data()).toList();
   }
 
   @override
@@ -126,27 +145,29 @@ class _AllCategoryScreenState extends State<AllCategoryScreen> {
             child: SingleChildScrollView(
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 20),
-                      createSearchCategory(
-                          context), // This is where your search widget will go
-                      const SizedBox(height: 10),
-                      //table goes here, for management
-                      FutureBuilder(
-                        future: fetchData(),
-                        builder: (context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
-                          if (snapshot.connectionState == ConnectionState.waiting) {
-                            return const CircularProgressIndicator();
-                          } else if (snapshot.hasError) {
-                            return Text('Error: ${snapshot.error}');
-                          } else {
-                            return buildDataTable(snapshot.data ?? []);
-                          }
-                        },
-                      ),
-                      ],
-                  ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    createSearchCategory(
+                        context), // This is where your search widget will go
+                    const SizedBox(height: 10),
+                    //table goes here, for management
+                    FutureBuilder(
+                      future: fetchData(),
+                      builder: (context,
+                          AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const CircularProgressIndicator();
+                        } else if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else {
+                          return buildDataTable(snapshot.data ?? []);
+                        }
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
